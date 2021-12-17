8e06484ad45751aaa87ff66bd649e84e5c3764fa
mark.bracke@tactics.be <mark.bracke@tactics.be>
2021-12-17 15:54:11 +0100

    Made day 3 more readable :)

--- Day3/Program.fs
+++ Day3/Program.fs
@@ -1,33 +1,35 @@
 module Day3
-open System
+
+type Move =
+    | Up
+    | Down
+    | Left
+    | Right
+    static member Parse =
+        function '^' -> Up | 'v' -> Down | '<' -> Left | '>' -> Right | other -> failwithf "Invalid character '%c'" other
 
 let readInput () =
     System.IO.File.ReadAllText(System.IO.Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory, "Day3", "Input.txt"))
+    |> Seq.map Move.Parse
 
 type Point =
     {
         X: int
         Y: int
     }
-    member me.Move (character: char) =
-        match character with
-        | '^' ->
-            { me with Y = me.Y + 1 }
-        | 'v' ->
-            { me with Y = me.Y - 1 }
-        | '>' ->
-            { me with X = me.X + 1 }
-        | '<' ->
-            { me with X = me.X - 1 }
-        | _ ->
-            failwithf "Unknown direction %c" character
+    member me.ExecuteMove =
+        function
+        | Up -> { me with Y = me.Y + 1 }
+        | Down -> { me with Y = me.Y - 1 }
+        | Right -> { me with X = me.X + 1 }
+        | Left -> { me with X = me.X - 1 }
 
 let part1 () =
     let startingPoint = { X = 0; Y = 0 }
     let mappedLocations =
         readInput ()
-        |> Seq.fold (fun (currentLocation: Point, map) nextCharacter ->
-            let newLocation = currentLocation.Move nextCharacter
+        |> Seq.fold (fun (currentLocation: Point, map: Map<Point, int>) (nextMove: Move) ->
+            let newLocation = currentLocation.ExecuteMove nextMove
             let newMap = map |> Map.change newLocation (function Some value -> Some (value + 1) | None -> Some 1)
             (newLocation, newMap)
         ) (startingPoint, Map [ (startingPoint, 1) ])
@@ -42,13 +44,13 @@ let part2 () =
     let mappedLocations =
         readInput ()
         |> Seq.indexed
-        |> Seq.fold (fun (santaLocation: Point, roboSantaLocation: Point, map) (index, move) ->
+        |> Seq.fold (fun (santaLocation: Point, roboSantaLocation: Point, map: Map<Point, int>) (index: int, move: Move) ->
             if index % 2 = 0 then
-                let newSantaLocation = santaLocation.Move move
+                let newSantaLocation = santaLocation.ExecuteMove move
                 let newMap = map |> Map.change newSantaLocation (function Some value -> Some (value + 1) | None -> Some 1)
                 (newSantaLocation, roboSantaLocation, newMap)
             else
-                let newRoboSantaLocation = roboSantaLocation.Move move
+                let newRoboSantaLocation = roboSantaLocation.ExecuteMove move
                 let newMap = map |> Map.change newRoboSantaLocation (function Some value -> Some (value + 1) | None -> Some 1)
                 (santaLocation, newRoboSantaLocation, newMap)
         ) (startingPoint, startingPoint, Map [ (startingPoint, 2) ])
