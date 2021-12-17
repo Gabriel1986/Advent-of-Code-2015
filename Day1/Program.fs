35a102b87c225d1ddfd45cc29b4ac903f0141196
mark.bracke@tactics.be <mark.bracke@tactics.be>
2021-12-17 15:40:11 +0100

    Made mapping character to floor movement easier to read

--- Day1/Program.fs
+++ Day1/Program.fs
@@ -3,15 +3,15 @@ module Day1
 let readInput () =
     System.IO.File.ReadAllText(System.IO.Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory, "Day1", "Input.txt"))
 
-let characterMap = Map [
-    '(', 1
-    ')', -1
-]
-let mapCharacter (character: char) = characterMap[character]
+let characterToFloorMovement = 
+    function 
+    | '(' -> 1 
+    | ')' -> -1 
+    | other -> failwithf "Unknown character: %c" other
 
 let part1 () =
     readInput()
-    |> Seq.sumBy mapCharacter
+    |> Seq.sumBy characterToFloorMovement
     |> printfn "Day 1 - Part 1 - Santa needs to go to floor %i"
 
 // Taking advantage that sequences are lazy, find will only calculate the sequence until it reaches the first hit
@@ -21,7 +21,7 @@ let part2 () =
     seq {
         let mutable currentFloor = 0
         for i in 0..input.Length-1 do
-            currentFloor <- currentFloor + mapCharacter (input[i])
+            currentFloor <- currentFloor + characterToFloorMovement (input[i])
             yield (i + 1, currentFloor)
     }
     |> Seq.find (fun (_, floor) -> floor = -1)
